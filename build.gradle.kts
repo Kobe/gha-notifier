55697c3389cba402d84047c36da0d4222705705f
Kobe <81110+Kobe@users.noreply.github.com>
2021-10-21 15:14:16 +0200

    go further to next compose alpha v4

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.21"
-    id("org.jetbrains.compose") version "1.0.0-alpha3"
+    kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.compose") version "1.0.0-alpha4-build411"
 }
 
 group = "de.kobe"
@@ -18,6 +18,7 @@ repositories {
 
 dependencies {
     implementation(compose.desktop.currentOs)
+
 }
 
 tasks.withType<KotlinCompile> {
